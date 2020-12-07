df036ee209a35d4e57afdf3a0506b420ea2aeb58
ferry <ferry@ferrys-MacBook-Air.local>
2020-12-07 21:33:32 +0700

    update

--- src/main/java/co/id/dasa/poolconn/demopollconn/ConnectionSta.java
+++ src/main/java/co/id/dasa/poolconn/demopollconn/ConnectionSta.java
@@ -23,7 +23,7 @@ public class ConnectionSta {
 
     private Connection connection = null;
 
-    private String database = "database";
+    private String database = "catalogsoa";
     private String hostname = "localhost";
     private String portnum = "3306";
     private String sid = "";
@@ -32,8 +32,7 @@ public class ConnectionSta {
     private String strurl = "jdbc:mysql://";
     private String strdrv = "com.mysql.cj.jdbc.Driver";
     private String user = "root";
-    private String pass = "Password";
-    
+    private String pass = "Jakarta1!";
     private String timezone = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
     // static variable single_instance of type Singleton 
@@ -80,7 +79,7 @@ public class ConnectionSta {
             ds.setUsername(user);
             ds.setPassword(pass);
  
-
+            
             // Parameters for connection pooling
             ds.setMinIdle(10);
             ds.setMaxIdle(30);
